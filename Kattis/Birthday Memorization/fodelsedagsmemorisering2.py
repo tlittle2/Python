c85e96d136e7d64cafbfe48d0d42b4a0dbb8c350
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-05-24 13:57:24 -0400

    Create fodelsedagsmemorisering2.py

---
+++ Kattis/Birthday Memorization/fodelsedagsmemorisering2.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+def main():
+    likes = {}
+    birthdays = defaultdict(list)
+
+    #birthday dict will group everyone who has the same birthday. likes will just be a dict of a person's likeness score
+    for i in range(int(input())):
+        name, like, birthday = input().split()
+     
+        birthdays[birthday].append(name)
+        likes[name] = int(like)
+
+        
+    ans = []
+    for v in birthdays.values(): #for each list entry in birthdays (birthday : [list of people])
+        
+        joinDict = {likes[i]: i for i in v} #"join" on birthday and likes on the basis of person
+
+        ans.append(joinDict[max(joinDict.keys())])# get max "like" for each group (guaranteed to be distinct according to problem)
+
+    print(len(ans))
+    for i in sorted(ans):
+        print(i)
+
+if __name__ == "__main__":
+    main()
