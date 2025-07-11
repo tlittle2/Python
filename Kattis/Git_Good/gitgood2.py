2dce304c828e17f16f50f481a3c36b3f6869e5aa
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-07-10 22:07:35 -0400

    Create gitgood2.py

---
+++ Kattis/Git_Good/gitgood2.py
@@ -0,0 +1,33 @@
+#accepted, but all test cases are not passed
+def main():
+    d = {}
+    curr_dir_files= set()
+    cur_dir_name = ""
+
+    def unload():
+        d[cur_dir_name] = [i for i in curr_dir_files]
+        curr_dir_files.clear()
+
+    for _ in range(int(input())):
+        #print(curr_dir)
+        ip = input().split(" ")
+        if ip[0] == 'cd':
+            unload()
+            cur_dir_name = ip[1]
+        else:
+            curr_dir_files.add(ip[1])
+
+    unload()
+
+    newD = dict(filter(lambda item: len(item[1]) > 0, d.items()))
+    #print(newD)
+
+    for k in sorted(newD.keys()):
+        for i in sorted(d[k]):
+            print("git add {}{}".format(k if k == "" else k +"/", i))
+
+    print("git commit")
+    print("git push")
+
+if __name__ == "__main__":
+    main()
