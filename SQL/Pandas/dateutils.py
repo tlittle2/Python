f2c21cf6c1d6f00fcf41ba714142cc8f4e60639e
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-11-23 16:24:45 -0500

    Refactor dateutils.py to use assertutils

--- SQL/Pandas/dateutils.py
+++ SQL/Pandas/dateutils.py
@@ -2,6 +2,7 @@ from pandas import DataFrame, DateOffset, Series
 from datetime import datetime
 import pdutils
 from numpy import sign
+import assertutils
 
 months_in_year: int = 12
 quarters_in_year: int = 4
@@ -12,35 +13,37 @@ year_quarter_sep: str = "Q"
 col_date = "date"
 col_year_quarter = "year_quarter"
 
-def __is_validate_year_quarter(year_quarter) -> bool:
-    return year_quarter[4] == year_quarter_sep and len(year_quarter) == 6
-
 def format_year_quarter(year: int, month: int) -> str:
     return "{}{}{}".format(year, year_quarter_sep, month)
 
 def get_quarter(month: int) -> int:
-    assert pdutils.is_between(month, min_calendar_value, months_in_year), "invalid month provided. investigate"
+    assertutils.is_valid_month(month, "invalid month provided. investigate")
     quarter = (month - min_calendar_value) // months_in_quarter + min_calendar_value
-    assert pdutils.is_between(quarter, min_calendar_value, quarters_in_year), "invalid quarter returned. investigate"
+    assertutils.is_valid_quarter(quarter, "invalid quarter returned. investigate")
     return quarter
 
+def get_quarter_from_date(date: datetime) -> str:
+    return get_quarter(date.month)
+
 def get_year_quarter(date: datetime) -> str:
-    return format_year_quarter(date.year, get_quarter(date.month))
+    return format_year_quarter(date.year, get_quarter_from_date(date))
 
 def get_new_year_quarter(year_quarter: str, num_of_quarters: int) -> str:
+    assertutils.is_valid_year_quarter(year_quarter)
     temp_date = get_min_date_for_year_quarter(year_quarter) + DateOffset(months=num_of_quarters * months_in_quarter)
     return get_year_quarter(temp_date)
 
 def get_year_from_year_quarter(year_quarter: str) -> int:
-    assert __is_validate_year_quarter(year_quarter)
+    assertutils.is_valid_year_quarter(year_quarter)
     return int(year_quarter[0:4])
 
 def get_quarter_from_year_quarter(year_quarter: str) -> int:
-    assert __is_validate_year_quarter(year_quarter)
+    assertutils.is_valid_year_quarter(year_quarter)
     return int(year_quarter[5])
 
 #====================================================================================================
 def get_min_date_for_year_quarter(year_quarter: str) -> datetime:
+    assertutils.is_valid_year_quarter(year_quarter)
     year: int = get_year_from_year_quarter(year_quarter)
     quarter: int = get_quarter_from_year_quarter(year_quarter)
     month: int = (quarter - min_calendar_value) * months_in_quarter + min_calendar_value
@@ -51,6 +54,7 @@ def get_min_date_for_quarter(date: datetime) -> datetime:
     return get_min_date_for_year_quarter(get_year_quarter(date))
 
 def get_max_date_for_year_quarter(year_quarter: str) -> datetime:
+    assertutils.is_valid_year_quarter(year_quarter)
     min_date = get_min_date_for_year_quarter(year_quarter)
 
     max_date = (min_date + DateOffset(months=months_in_quarter)) - DateOffset(days=min_calendar_value)
@@ -63,7 +67,8 @@ def get_max_date_for_quarter(date: datetime) -> datetime:
 #====================================================================================================
 
 def __is_quarter(year_quarter: str, q_num: int) -> bool:
-    assert pdutils.is_between(q_num, min_calendar_value, quarters_in_year), "invalid quarter provided. investigate"
+    assertutils.is_valid_year_quarter(year_quarter)
+    assertutils.is_valid_quarter(q_num, "invalid quarter provided. investigate")
     return get_quarter_from_year_quarter(year_quarter) == q_num
 
 def is_quarter1_yq(year_quarter: str) -> bool:
@@ -95,7 +100,7 @@ def is_quarter4_dt(date: datetime) -> bool:
 
 
 def get_days(start_date: datetime, num_of_days: int)-> DataFrame:
-    s = Series([start_date + DateOffset(days = sign(num_of_days) * i) for i in range(abs(num_of_days))] )
+    s = Series([start_date + DateOffset(days = sign(num_of_days) * i) for i in range(abs(num_of_days))])
     return DataFrame({col_date: s})
 
 def get_dates(start_date: datetime, end_date: datetime)-> DataFrame:
@@ -103,8 +108,7 @@ def get_dates(start_date: datetime, end_date: datetime)-> DataFrame:
     return get_days(start_date, days)
 
 def get_year_quarters(start_quarter: str, num_of_quarters: int)-> DataFrame:
-    assert __is_validate_year_quarter(start_quarter), "invalid starting year quarter provided. please investigate"
-
-    s = Series([get_new_year_quarter(start_quarter, sign(num_of_quarters) * i) for i in range(abs(num_of_quarters))] )
+    assertutils.is_valid_year_quarter(start_quarter)
+    s = Series([get_new_year_quarter(start_quarter, sign(num_of_quarters) * i) for i in range(abs(num_of_quarters))])
 
     return DataFrame({col_year_quarter: s})
